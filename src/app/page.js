9a2514b49b421b394aadb7923b271c2b8e3d64fc
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2024-09-07 11:13:10 +0600

    Understanding Layout and Page

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import Heading from "@/components/Heading";
 import Image from "next/image";
 
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1>Hello Next Hero</h1>
       <button>Hi</button>
+      <Heading></Heading>
     </div>
   );
 }
