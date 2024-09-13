ef241dfb5171c9ba881da7a3628c6489369f39bf
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2024-09-13 12:00:03 +0600

    Understanding Of Next.js Nested Routing

--- src/components/AboutContent.jsx
+++ src/components/AboutContent.jsx
@@ -1,9 +1,14 @@
+import Link from 'next/link';
 import React from 'react';
 
 const AboutContent = () => {
     return (
         <div>
-            <h1>About Contents </h1>
+            <h1>About Contents Page</h1> <br />
+            <div className='flex flex-col space-y-5 mb-9'>
+                <Link href={'/about/history'}>History</Link>
+                <Link href={'/about/mission'}>Mission</Link>
+            </div>
         </div>
     );
 };
