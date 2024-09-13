5a31c407ac6873e7450d42fb00381568b2fa2662
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2024-09-13 15:14:34 +0600

    Next.js Catch All Segments Concept Understanding

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,6 +23,10 @@ const Navbar = () => {
             title: "Blogs",
             path: "/blogs"
         },
+        {
+            title: "Categories",
+            path: "/categories"
+        },
     ]
     const handler = () => {
         router.push('/login')
